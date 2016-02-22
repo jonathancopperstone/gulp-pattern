be7c00256bd436d99ae603db38a84d51e5508e20
Johnny Copperstone <jonnce@gmail.com>
2016-02-22 16:42:55 +0000

    Add pattern config. Ignore tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,15 @@ var gulp = require('gulp-help')(require('gulp')),
 // The main config file for your
 // application.
 
-var config  = require('./gulp/configs/app.conf');
+var config = require('./gulp/configs/app.conf'),
+    patternConfig = {
+        tasks: {
+            ignore: ['default', 'help']
+        }
+    };
 
 // Setup the pattern helper to go
 // through your directory and add the
 // gulp tasks and workflows.
 
-var pattern = require('./index').setup(gulp, args, config);
+var pattern = require('./index').setup(gulp, args, config, patternConfig);
